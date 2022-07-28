2a9b87e6924e6b0332c42c1d92c22589cd196953
Aquib Vadsaria <vadsaria.ak@gmail.com>
2022-07-28 21:14:58 +0530

    add api/hello/ api with helloAPIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloAPIView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Return list of API Views"""
+        sample_data = [
+            "Apple",
+            "Banana",
+            "Strawberry",
+            "Water Melon"
+        ]
+
+        return Response({
+            "sample":  sample_data
+        })
